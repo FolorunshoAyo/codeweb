b8db22b752d2869d13cc6bc8b4245f54e0041150
dabirideji <dabirideji@gmail.com>
2023-01-29 19:37:22 -1100

    fixed bugs on the sign-in page and in the sign-in process
    the code wasnt redirecting the user properly so i had to make it redirect from the sign-in process. code is open to correction by whomsoever

--- student/controllers/sign-in-process.php
+++ student/controllers/sign-in-process.php
@@ -13,24 +13,33 @@ if (isset($_POST['submit'])) {
 		if ($sql->num_rows == 1) {
 			$row = $sql->fetch_assoc();
 			$passcode = $row['password'];
-			// if (password_verify($password, $passcode)) {
-			if ($password === $passcode) {
-				if($row['has_paid_form'] == 0) {
+			if (password_verify($password, $passcode)) {
+			// if ($password === $passcode) {
+				if($row['has_paid_form'] == '0' && $row['is_applicant']=='0') {
 					//this user had not paid for the application form
 					//sent to payment gateway to pay for application form
-					echo json_encode(array('success' => 1, 'redirect[' => 'make_payment'));
+					json_encode(array('success' => 1, 'redirect' => 'make_payment'));
+					header("Location:../make-payment.php");
 				}elseif ($row['has_paid_form'] == '1' && $row['is_applicant'] == '0') {
 					//this user is made payment for the application form but has not filled the form.
 					//redirect user to the application form
-					echo json_encode(array('success' => 1, 'redirect' => 'application_form'));
+					json_encode(array('success' => 1, 'redirect' => 'application_form'));
+					header("Location:../make-payment.php");
+					
+
 				}elseif ($row['is_applicant'] == '1' && $row['is_student']=='0') {
 					//this person has filled  the application form but has not selected any course of study
 					//redirect to select course page
-					echo json_encode(array('success' => 1, "redirect" => "select_course"));
+					 json_encode(array('success' => 1, "redirect" => "select_course"));
+					header("Location:../select-course.php");
+
 				}elseif ($row['is_applicant'] == '1' && $row['is_student']=='1') {
 					//this  user has selected a course of study and has made payments.
-					echo json_encode(array('success' => 1, "redirect" => "student_dashboard"));
+					 json_encode(array('success' => 1, "redirect" => "student_dashboard"));
+					header("Location:../student_dashboard.php");
+
 				}
+				
                 
 			}else {
 				//Error incorrect password
@@ -41,6 +50,7 @@ if (isset($_POST['submit'])) {
 		echo json_encode(array('success' => 0, "error_title" => "incorrect details"));
 		}
 	}
+// }
 }else {
 	//Error if not isset
 	echo json_encode(array('success' => 0, "error_title" => "fatal"));
--- student/sign-in.php
+++ student/sign-in.php
@@ -1,10 +1,11 @@
-<?php 
+<?php
 // require(__DIR__.'/auth-library/resources.php');
 // Auth::Route("student/");
 // $url = strval($url);
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -36,23 +37,25 @@
                 <h1 class="title">Sign in</h1>
 
                 <div class="registeration-form-container">
-                    <form action="controllers/sign-up-process.php" method="post" id="registeration-form">
-                        
-                    <div class="form-groupings">
-                        <div class="form-group-container w-100">
-                            <div class="form-group animate">
-                                <input type="text" name="username" id="username" class="form-input"  placeholder=" " required>
-                                <label for="username">Username</label>
-                            </div>
-                        </div>
+                    <form action="controllers/sign-in-process.php" method="post" id="registeration-form">
 
-                        <div class="form-group-container w-100">
-                            <div class="form-group animate">
-                                <input type="password" name="pwd" id="pwd" class="form-input"  placeholder=" " required>
-                                <label for="pwd">Password</label>
+                        <div class="form-groupings">
+                            <div class="form-group-container w-100">
+                                <div class="form-group animate">
+                                    <input type="text" name="username" id="username" class="form-input" placeholder=" "
+                                        required>
+                                    <label for="username">Username</label>
+                                </div>
+                            </div>
+
+                            <div class="form-group-container w-100">
+                                <div class="form-group animate">
+                                    <input type="password" name="pwd" id="pwd" class="form-input" placeholder=" "
+                                        required>
+                                    <label for="pwd">Password</label>
+                                </div>
                             </div>
                         </div>
-                    </div>
 
                         <div class="forgot-password-container">
                             <a href="#">Forgot password?</a>
@@ -94,153 +97,74 @@
         });
 
         validation
-        // .addField('#fname', [
-        //     {
-        //         rule: 'required',
-        //         errorMessage: "Field is required"
-        //     },
-        //     {
-        //     rule: 'minLength',
-        //     value: 3,
-        //     },
-        //     {
-        //     rule: 'maxLength',
-        //     value: 30,
-        //     },
-        // ])
-        // .addField('#lname', [
-        //     {
-        //         rule: 'required',
-        //         errorMessage: "Field is required"
-        //     },
-        //     {
-        //     rule: 'minLength',
-        //     value: 3,
-        //     },
-        //     {
-        //     rule: 'maxLength',
-        //     value: 30,
-        //     },
-        // ])
-        .addField('#username', [
-            {
-            rule: 'required',
-            errorMessage: 'Field is required',
-            },
-        ])
-        // .addField('#pnum', [
-        //     {
-        //         rule: 'required',
-        //         errorMessage: "Field is required"
-        //     },
-        //     {
-        //     rule: 'minLength',
-        //     value: 11,
-        //     },
-        //     {
-        //     rule: 'maxLength',
-        //     value: 11,
-        //     },
-        // ])
-        .addField('#pwd', [
-            {
-                rule: 'minLength',
-                value: 6,
-            },
-            {
-                rule: 'required',
-                errorMessage: "Please provide a password"
-            }
-        ])
-        // .addField('#cpwd', [
-        //     {
-        //         rule: 'minLength',
-        //         value: 6,
-        //     },
-        //     {
-        //         rule: 'required',
-        //         errorMessage: "Field is required"
-        //     },
-        //     {
-        //         validator: (value, fields) => {
-        //         if (fields['#pwd'] && fields['#pwd'].elem) {
-        //             const repeatPasswordValue = fields['#pwd'].elem.value;
-
-        //             return value === repeatPasswordValue;
-        //         }
-
-        //         return true;
-        //         },
-        //         errorMessage: 'Passwords should be the same',
-        //     }
-        // ])
-        // .addField('#agree_to_terms', [
-        //     {
-        //         rule: 'required',
-        //         errorMessage: 'Please agree to the terms'
-        //     }
-        // ])
-        .onSuccess(() => {
-            const form = document.getElementById('registeration-form');
-
-            // GATHERING FORM DATA
-            const formData = new FormData(form);
-            formData.append("submit", true);
-            
-            //SENDING FORM DATA TO THE SERVER
-            $.ajax({
-                type: "post",
-                url: 'controllers/sign-in-process.php',
-                data: formData,
-                cache: false,
-                contentType: false,
-                enctype: 'multipart/form-data',
-                processData: false,
-                dataType: 'json',
-                beforeSend: function () {
-                    $(".register-container button").html("Registering...");
-                    $(".register-container button").attr("disabled", true);
+          
+            .addField('#username', [
+                {
+                    rule: 'required',
+                    errorMessage: 'Field is required',
                 },
-                success: function (response) {
-                setTimeout(() => {
-                        if (response.success === 1) {
-                            // REDIRECT USER TO THE VERIFICATION PAGE
-                            // window.location = "authentication/send-code?a=send";
-                            if(response.redirect=='make_payment'){
-                                window.location.href='make-payment.php'
-                            }
-                            if(response.redirect=='application_form'){
-                                window.location.href='enroll.php'
-                            }
-                            if(response.redirect=='select_course'){
-                                window.location.href='select_course.php'
-                            }
-                            if(response.redirect=='student_dashboard'){
-                                window.location.href='student_dashboard.php'
-                            }
+            ])
+          
+            .addField('#pwd', [
+                {
+                    rule: 'minLength',
+                    value: 6,
+                },
+                {
+                    rule: 'required',
+                    errorMessage: "Please provide a password"
+                }
+            ])
+           
+            .onSuccess(() => {
+                const form = document.getElementById('registeration-form');
 
-                        } else {
-                            $(".register-container button").setAttr("disabled", false);
-                            $(".register-container button").html("Register");
+                // GATHERING FORM DATA
+                const formData = new FormData(form);
+                formData.append("submit", true);
 
-                            if(response.error_title === "fatal"){
+                //SENDING FORM DATA TO THE SERVER
+                $.ajax({
+                    type: "post",
+                    url: 'authentication/register.php',
+                    data: formData,
+                    cache: false,
+                    contentType: false,
+                    enctype: 'multipart/form-data',
+                    processData: false,
+                    dataType: 'json',
+                    beforeSend: function () {
+                        $(".register-container button").html("Registering...");
+                        $(".register-container button").attr("disabled", true);
+                    },
+                    success: function (response) {
+                        setTimeout(() => {
+                            if (response.success === 1) {
+                                // REDIRECT USER TO THE VERIFICATION PAGE
+                                window.location = "authentication/send-code?a=send";
+                               } else {
+                                     $(".register-container button").setAttr("disabled", false);
+                                    $(".register-container button").html("Register");
+ 
+                                if(response.error_title === "fatal"){
                                 // REFRESH CURRENT PAGE
-                                location.reload();
-                            }else{
-                                // ALERT USER
-                                Swal.fire({
+                                    location.reload();
+                                 }else{
+                                     // ALERT USER
+                                      Swal.fire({
                                     title: response.error_title,
-                                    icon: "error",
+                                      icon: "error",
                                     text: response.error_message,
-                                    allowOutsideClick: false,
+                                      allowOutsideClick: false,
                                     allowEscapeKey: false,
-                                });
-                            }
-                        }
-                    }, 1500);
-                },
-            });
-        });
+                                        });
+                                            }
+                                       }
+
+    }, 1500);
+    },
+    });
+    });
     </script>
 </body>
 
